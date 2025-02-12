21350d4a35e693b9cfe8a913c2527ed580cca43f
Cside <cside.story@gmail.com>
2025-02-13 00:12:54 +0900

    なぜかクラッシュするので。。

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,7 +1,8 @@
 export default defineBackground(() => {
   chrome.runtime.onInstalled.addListener(async ({ reason }) => {
     if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
-      await chrome.action.openPopup();
+      // await chrome.action.openPopup(); // TODO なぜかクラッシュする。。
+      await chrome.runtime.openOptionsPage();
     }
   });
 });
