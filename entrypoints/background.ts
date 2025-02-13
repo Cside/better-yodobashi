06ac662ac24a10dd59f98f82cd79585cb52da07f
Cside <cside.story@gmail.com>
2025-02-13 22:46:36 +0900

    横幅をset

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,8 +1,8 @@
 export default defineBackground(() => {
   chrome.runtime.onInstalled.addListener(async ({ reason }) => {
     if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
-      await chrome.action.openPopup(); // TODO なぜかクラッシュする。。
-      // await chrome.runtime.openOptionsPage();
+      // await chrome.action.openPopup(); // TODO クラッシュしなくなったけど、怖いから一応やめておく
+      await chrome.runtime.openOptionsPage();
     }
   });
 
--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -9,6 +9,7 @@ Sentry.init({
   dsn: import.meta.env.DEV
     ? "https://d6f04db311083810b1aecdd573f37829@o49171.ingest.us.sentry.io/4508812083855360" // dev
     : "https://08c169747ea6e0e0d1065195518a102d@o49171.ingest.us.sentry.io/4508812084903936", // prod
+  replaysSessionSampleRate: 0.1, // replay をサンプリングしないと Sentry が警告を吐きやがるので、仕方なく
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration(),
