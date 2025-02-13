a9ad38b1e19c8beee33e1a94c6a41070aea802a4
Cside <cside.story@gmail.com>
2025-02-14 00:16:12 +0900

    locale

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   extensionApi: "chrome",
   modules: ["@wxt-dev/module-react"],
   manifest: {
+    default_locale: "ja", // for edge
     permissions: ["storage", "unlimitedStorage"],
     action: {}, // entrypoints/popup がある場合は不要になる
   },
